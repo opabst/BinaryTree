deedb7eb8447a103aa35d7de0f8be3814e1624f8
Oliver Pabst <oliver.pabst@mailbox.org>
2018-11-14 14:52:40 +0100

    rekursiven Postorder-Durchlauf korrgiert

--- src/main/java/de/oliverpabst/runs/PostorderRun.java
+++ src/main/java/de/oliverpabst/runs/PostorderRun.java
@@ -3,15 +3,16 @@ package de.oliverpabst.runs;
 import de.oliverpabst.adts.BinaryTree;
 import de.oliverpabst.adts.BinaryTreeNode;
 
+import java.util.ArrayList;
+
 public class PostorderRun<T>
 {
     public void postorder(BinaryTree<T> tree, BinaryTreeNode<T> startNode) {
-        if(tree.isExternal(startNode)) {
-            System.out.println(startNode.toString());
-        } else {
-            postorder(tree, startNode.getLeftChild());
-            postorder(tree, startNode.getRightChild());
+        ArrayList<BinaryTreeNode<T>> children = tree.getChildren(startNode);
+        for(BinaryTreeNode<T> child: children) {
+            postorder(tree, child);
         }
+        System.out.println(startNode.toString());
     }
 
     public void postorderIter(BinaryTree<T> tree, BinaryTreeNode<T> startNode) {
